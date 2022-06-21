7a28985a186a5a09a6c369d140b239db0ed7fed9
reshator <durrajt@gmail.com>
2022-06-21 18:18:28 +0500

    optimize

--- app/src/main/java/com/example/meditation/recadapters/MyData.kt
+++ app/src/main/java/com/example/meditation/recadapters/MyData.kt
@@ -5,8 +5,8 @@ import com.example.meditation.R
 data class feel(val image: Int, val name_feel: String)
 class MyFeel {
     val list = arrayListOf(
-        feel(R.drawable.calm, "rajik"),
-        feel(R.drawable.focus, "Сас"),
+        feel(R.drawable.calm, "Спокойным"),
+        feel(R.drawable.focus, "Сфокусированным"),
         feel(R.drawable.anxious, "Тревожным"),
         feel(R.drawable.relax, "Chill"),
         feel(R.drawable.calm, "Обычным")
@@ -19,6 +19,6 @@ class MyState {
     val state_list = arrayListOf(
         state(R.drawable.state_image1, "Вставить текст", "Кратенькое описание блока с двумя строчками"),
         state(R.drawable.state_image2, "Вставить текст", "Кратенькое описание блока с двумя строчками"),
-        state(R.drawable.state_image1, "Как отдохнуть?", "Отдых с раджиком")
+        state(R.drawable.state_image1, "Как отдохнуть?", "Кратенькое описание блока с двумя строчками")
     )
 }
